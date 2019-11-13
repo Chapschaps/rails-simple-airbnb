011d4e5f0ddbab9891be390ba231de08b7769208
Leo Chaput <leo.chaput@hotmail.fr>
2019-11-13 18:28:35 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Flat.destroy_all
+puts 'Creating 10 fake appartments...'
+10.times do
+  flat = Flat.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description:  'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(10..50),
+    number_of_guests: rand(1..5)
+  )
+  flat.save!
+end
+puts 'Finished!'
